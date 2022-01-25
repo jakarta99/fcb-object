ff8b02fa667a68ca0e9c719d72244a8df32e3a6a
Gary Lee <jakarta99@gmail.com>
2022-01-25 17:38:29 +0800

    for String Test

---
+++ src/main/java/tw/com/fcb/sample/object/Song.java
@@ -0,0 +1,17 @@
+package tw.com.fcb.sample.object;
+
+public class Song {
+	
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
