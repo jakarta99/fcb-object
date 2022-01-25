ca0571cd49cd10816fbee08e8eee7f4860a65a45
Gary Lee <jakarta99@gmail.com>
2022-01-25 16:02:03 +0800

    init ObjectApp

---
+++ src/main/java/tw/com/fcb/sample/object/ObjectApp.java
@@ -0,0 +1,13 @@
+package tw.com.fcb.sample.object;
+
+public class ObjectApp {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 1;
+		
+		System.out.println(a == b);
+		
+	}
+
+}
