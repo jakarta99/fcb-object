a806d9ffedddf1627856ddd74c31dcaa1ebd266e
Gary Lee <jakarta99@gmail.com>
2022-01-25 16:29:19 +0800

    modify GaryApp

--- src/main/java/tw/com/fcb/sample/gary/GaryApp.java
+++ src/main/java/tw/com/fcb/sample/gary/GaryApp.java
@@ -3,8 +3,7 @@ package tw.com.fcb.sample.gary;
 public class GaryApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello Gary");
 	}
 
 }
