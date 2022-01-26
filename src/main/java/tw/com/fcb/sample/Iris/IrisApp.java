2386128195eff760c292f1558d37e44fc8445dfe
iris <dragoniris7@gmail.com>
2022-01-27 00:13:40 +0800

    del 註解

--- src/main/java/tw/com/fcb/sample/Iris/IrisApp.java
+++ src/main/java/tw/com/fcb/sample/Iris/IrisApp.java
@@ -8,27 +8,27 @@ public class IrisApp {
 		int numData = 12;
         System.out.println(String.format("%08d",numData));
         
-//        Scanner strNum = new Scanner(System.in);
-//        System.out.println("請輸入任意數字：");
-//        String dataNum = strNum.nextLine();
-//        System.out.println("請輸入資料呈現長度(右靠左補零)：");
-//        boolean flag = true;
-//        do{
-//            try{
-//                String inNum = strNum.nextLine();
-//                int len = Integer.valueOf(inNum);
-//                if (dataNum.length() > len ){
-//                    System.out.println("資料呈現長度不得小於(" + dataNum + ")長度=" + dataNum.length());
-//                }else{
-//                    GetFormat strF1 = new GetFormat();
-//                    strF1.setLen(len);
-//                    System.out.println("out=" + strF1.formatZeros(dataNum));
-//                    break;
-//                }
-//            } catch(NumberFormatException e){
-//                System.out.println("資料呈現長度非數字型態!請重新輸入「資料呈現長度(右靠左補零)」：");
-//            }
-//        }while (flag);
+        Scanner strNum = new Scanner(System.in);
+        System.out.println("請輸入任意數字：");
+        String dataNum = strNum.nextLine();
+        System.out.println("請輸入資料呈現長度(右靠左補零)：");
+        boolean flag = true;
+        do{
+            try{
+                String inNum = strNum.nextLine();
+                int len = Integer.valueOf(inNum);
+                if (dataNum.length() > len ){
+                    System.out.println("資料呈現長度不得小於(" + dataNum + ")長度=" + dataNum.length());
+                }else{
+                    GetFormat strF1 = new GetFormat();
+                    strF1.setLen(len);
+                    System.out.println("out=" + strF1.formatZeros(dataNum));
+                    break;
+                }
+            } catch(NumberFormatException e){
+                System.out.println("資料呈現長度非數字型態!請重新輸入「資料呈現長度(右靠左補零)」：");
+            }
+        }while (flag);
 
 	}
 
