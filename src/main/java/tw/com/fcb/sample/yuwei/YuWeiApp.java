69c60f9bb82e7db7440491503d00d3e919c0d146
YUWEI <simon81029168@gmail.com>
2022-01-27 10:00:06 +0800

    update homework 2022/01/27

--- src/main/java/tw/com/fcb/sample/yuwei/YuWeiApp.java
+++ src/main/java/tw/com/fcb/sample/yuwei/YuWeiApp.java
@@ -14,48 +14,11 @@ public class YuWeiApp {
 		System.out.println("please input total length:");
 		Scanner scanner = new Scanner(System.in);
 		int length= scanner.nextInt();
-		YuWeiApp yuWeiApp = new YuWeiApp();
-		result = yuWeiApp.leftPadding(num,length,result);
+		NumericProcess numericProcess = new NumericProcess();
+		result = numericProcess.leftPadding(num,length,result);
 		System.out.println(result);
-		result = yuWeiApp.rightPadding(num,length,result);
+		result = numericProcess.rightPadding(num,length,result);
 		System.out.println(result);
-	}
-	
-	
-	/**
-	 * 左補0程式
-	 * @param inputNum 輸入字串
-	 * @param length 總長度
-	 * @param result 結果
-	 * @return
-	 */
-	public String leftPadding(int inputNum,int length,String result) {
-
-		String pattern = "%"+length+"s";
-		result = String.format(pattern, inputNum).replace(' ', '0');
-		
-		//使用StringUtils
-//		result = StringUtils.leftPad(String.valueOf(inputNum), length).replace(' ', '0');
-		return result;
-	}
-	
-	/**
-	 * 右補0程式
-	 * @param inputNum 輸入字串
-	 * @param length 總長度
-	 * @param result 結果
-	 * @return
-	 */
-	public String rightPadding(int inputNum,int length,String result) {
-
-		String pattern = "%-"+length+"s";
-		result = String.format(pattern, inputNum).replace(' ', '0');
-		
-		//使用StringUtils
-//		result = StringUtils.rightPad(String.valueOf(inputNum), length).replace(' ', '0');
-		
-		return result;
-		
 	}
 	
 }
