7ceee8d0fb2c2f187dd103280927637257eea6b0
LAPTOP-6AEJPV82\user <a24562327@gmail.com>
2022-01-27 16:48:32 +0800

    Merge branch 'main' of https://github.com/jakarta99/fcb-object.git into main

--- src/main/java/tw/com/fcb/sample/WeiFan/WeiFan.java
+++ src/main/java/tw/com/fcb/sample/WeiFan/WeiFan.java
@@ -1,10 +1,26 @@
 package tw.com.fcb.sample.WeiFan;
 
+import java.util.Scanner;
+
 public class WeiFan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!!");
+		Scanner s = new Scanner(System.in);
+		System.out.println("請輸入數字：");
+		String Number = s.nextLine();
+		System.out.println("請輸入數字長度：");
+		int Len = s.nextInt();
+		if(Len >= Number.length()) {
+			String Z = "";
+			for(int i = 0 ; i < Len - Number.length(); i++) {
+				Z = Z+ "0";
+			}
+			Number = Z +Number;
+		}else {
+			System.out.println("長度輸入錯誤，無法補零");
+		}
+		System.out.println(Number);
 	}
 
 }
