9bb1d41f3c46f43f38c5168303216d854351dbbf
jilldolala25 <jilldolala25@gmail.com>
2022-01-27 14:23:37 +0800

    update

--- src/main/java/tw/com/fcb/sample/jilldolala25/Jilldolala25App.java
+++ src/main/java/tw/com/fcb/sample/jilldolala25/Jilldolala25App.java
@@ -2,13 +2,14 @@ package tw.com.fcb.sample.jilldolala25;
 
 import tw.com.fcb.sample.object.Song;
 
+import java.security.spec.ECField;
 import java.util.Scanner;
 
 public class Jilldolala25App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         int input;
         int input1;
-
+        try {
             System.out.print("請輸入一個數值:");
             Scanner scanner = new Scanner(System.in);
             input = scanner.nextInt();
@@ -16,13 +17,15 @@ public class Jilldolala25App {
             Scanner scanner1 = new Scanner(System.in);
             input1 = scanner1.nextInt();
             NumericProcess numericProcess = new NumericProcess();
-            System.out.println("DecimalFormat   = " + numericProcess.DecimalFormat(input,input1));
-            System.out.println("SubStringFormat = " + numericProcess.SubStringFormat(String.valueOf(input),input1));
-            System.out.println("StringFormat    = " + numericProcess.StringFormat(input,input1));
+            System.out.println("DecimalFormat   = " + numericProcess.DecimalFormat(input, input1));
+            System.out.println("SubStringFormat = " + numericProcess.SubStringFormat(String.valueOf(input), input1));
+            System.out.println("StringFormat    = " + numericProcess.StringFormat(input, input1));
             System.out.println("paddingLeft     = " + numericProcess.PaddingLeft(String.valueOf(input), input1));
-            System.out.println("contactProcess  = " + numericProcess.ContactProcess("dog","cat"));
+            System.out.println("contactProcess  = " + numericProcess.ContactProcess("dog", "cat"));
+        }catch (Exception e) {
+            System.out.println("輸入錯誤!!");
 
-
+        }
 
     }
 }
